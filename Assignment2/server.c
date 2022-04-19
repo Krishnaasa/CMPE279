6b71eabf1bbada46fed80666ddbfdeb2f1c19c55
Krishnaa <krishna201294@gmail.com>
2022-04-18 21:13:08 -0700

    Changes to Re-executing child process after fork

--- Assignment2/server.c
+++ Assignment2/server.c
@@ -22,44 +22,67 @@ int main(int argc, char const *argv[])
 
     // Show ASLR
     printf("execve=0x%p\n", execve);
+    if(argc == 1 ){
 
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
+        // Creating socket file descriptor
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+        {
+            perror("socket failed");
+            exit(EXIT_FAILURE);
+        }
 
-    // Attaching socket to port 80
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR,
-                   &opt, sizeof(opt)))
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+        // Attaching socket to port 80
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR,
+                    &opt, sizeof(opt)))
+        {
+            perror("setsockopt");
+            exit(EXIT_FAILURE);
+        }
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons( PORT );
 
-    // Forcefully attaching socket to the port 80
-    if (bind(server_fd, (struct sockaddr *)&address,
-                                 sizeof(address))<0)
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    printf("Original UID is %d \n", getuid());
+        // Forcefully attaching socket to the port 80
+        if (bind(server_fd, (struct sockaddr *)&address,
+                                    sizeof(address))<0)
+        {
+            perror("bind failed");
+            exit(EXIT_FAILURE);
+        }
+        if (listen(server_fd, 3) < 0)
+        {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                (socklen_t*)&addrlen))<0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        printf("Original UID is %d \n", getuid());
 
-    pid_t child_pid = fork();
-    if(child_pid<0){
-        perror("child failed");
-        exit(EXIT_FAILURE);
+        pid_t child_pid = fork();
+        if(child_pid<0){
+            perror("child failed");
+            exit(EXIT_FAILURE);
+        }
+        else if(child_pid == 0){
+            char socket_address[12];
+            sprintf(socket_address, "%d", new_socket);
+
+            char * paramsList[] = {"./server", socket_address, NULL};
+            if(execvp(paramsList[0], paramsList) < 0){
+            perror("exec");
+            exit(EXIT_FAILURE);
+            }
+        }
+        if(waitpid(child_pid, &childp_status, 0) < 0){
+            perror("waitpid");
+            exit(EXIT_FAILURE);
+        }
     }
+    
     else{
         pw = getpwnam("nobody"); // getting UID for nobody
         printf("Forked UID for user nobody is %ld \n", (long)pw->pw_uid);
@@ -72,16 +95,12 @@ int main(int argc, char const *argv[])
 
         if(cp_uid == 0)
         {
-            if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                (socklen_t*)&addrlen))<0){
-                    perror("accept");
-                    exit(EXIT_FAILURE);
-                }
-                valread = read( new_socket , buffer, 1024);
-                printf("%s\n",buffer );
-                send(new_socket , hello , strlen(hello) , 0 );
-                printf("Hello message sent\n");
-                return 0;
+            new_socket = atoi(argv[1]);
+            valread = read( new_socket , buffer, 1024);
+            printf("%s\n",buffer );
+            send(new_socket , hello , strlen(hello) , 0 );
+            printf("Hello message sent\n");
+            return 0;
         }
         else
         {
@@ -89,9 +108,6 @@ int main(int argc, char const *argv[])
             exit(EXIT_FAILURE);
         }
     }
-    if(waitpid(child_pid, &childp_status, 0) < 0){
-        perror("waitpid");
-        exit(EXIT_FAILURE);
-    }
+    
     return 0;
 }
